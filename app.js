b848fe137ae9a3be29d90df47e456c91a02f6ce9
Ishanka Withanage <ishankawithanage@Ishankas-MacBook-Air.local>
2021-01-25 19:25:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = require("./routes/recipes");
-const avengers = require("./routes/recipes");
+const recipes = require("./routes/recipes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/recipes", avengers);
+app.use("/recipes", recipes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Personal Recipes");
